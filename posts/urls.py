2508bf03c63b82d0f0be22dd7c849d03e5457f08
Adeyinka Adewojo <44239661+adeywojo@users.noreply.github.com>
2021-06-30 09:08:36 -0500

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('', PostListView.as_view(), name='home'),
     path('new/', views.create_post, name='create-post'),
     path('<pk>/update', views.update_post, name='update-post'),
+    path('update/<int:pk>', views.update_post, name='update-post'),
     path('delete/<int:pk>', views.post_delete, name='delete'),
+    path('comment/<int:pk>', views.post_detail, name='comment'),
+    path('comment/delete/<int:pk>', views.delete_comment, name='delete-comment'),
 
 ]
