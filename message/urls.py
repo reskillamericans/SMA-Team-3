8a492c9d2c2295f2f8331a59e84274d0ac3fde40
Ashlyn Strain <ms.lynash@gmail.com>
2021-07-06 19:23:10 -0400

    Message Features

--- message/urls.py
+++ message/urls.py
@@ -7,8 +7,8 @@ from .views import MessageThreadList, CreateMessage, CreateMessageThread, Messag
 app_name = 'message'
 url_pattern = [
     path('', MessageThreadList.as_view(), name = 'inbox'),
-    path('new-thread/', CreateMessageThread.as_view(), name = 'create-message-thread'),
-    path('view-thread/<int:pk>/', MessageThreadView.as_view(), name = 'message-thread'),
+    path('new-message-thread/', CreateMessageThread.as_view(), name = 'create-message-thread'),
+    path('message-thread/<int:pk>/', MessageThreadView.as_view(), name = 'message-thread'),
     path('message/<int:pk>/', CreateMessage.as_view(), name = 'message'),
-    path('message-thread/delete/<int:pk>', views.delete_message_thread, name = 'delete-message-thread')
+    path('delete/message-thread/<int:pk>', views.delete_message_thread, name = 'delete-message-thread')
 ] 
